6d4336126e5e3be730692a7e86eb1b909adad3ad
Kiki Rizki Arpiandi <kkrzkrk@gmail.com>
2017-09-28 03:00:52 +0700

    Edit download frozen graph first

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ then
     echo "the frozen graph exist no need to download"
 else
     wget https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz
-    tar inception_v3_2016_08_28_frozen.pb.tar.gz 
+    sudo tar inception_v3_2016_08_28_frozen.pb.tar.gz 
 fi
 g++ -std=c++11 -Wl,-rpath='$ORIGIN/lib' -Iinclude -Llib label_image.cpp -ltensorflow_cc -o exec
